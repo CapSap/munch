c4105aec5ce669c307af33108936c86393f20f74
CapSap <cshotam@gmail.com>
2025-12-29 08:55:22 +1100

    wip

--- components/RecipeCard.tsx
+++ components/RecipeCard.tsx
@@ -1,29 +1,73 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleProp, StyleSheet, Text, View, ViewStyle } from 'react-native';
+import { card } from './Recipes';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 
 export default function RecipeCard(props: {
   backgroundColour: string;
-  transform: string;
+  data: card;
+  isTopCard: boolean;
+  rotate: number;
 }) {
-  console.log(props.backgroundColour);
+  const isPressed = useSharedValue(false);
+  const offset = useSharedValue({ x: 0, y: 0 });
+
+  const gesture = Gesture.Pan()
+    .onBegin(() => {
+      isPressed.value = true;
+    })
+    .onUpdate(e => {
+      offset.value = {
+        x: e.translationX,
+        y: e.translationY,
+      };
+      console.log('Offset:', offset.value.x); // This will log (but may be slow)
+    })
+    .onEnd(() => {
+      offset.value = {
+        x: 0,
+        y: 0,
+      };
+    })
+    .onFinalize(() => {
+      isPressed.value = false;
+    });
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: offset.value.x },
+        { translateY: offset.value.y },
+        { scale: withSpring(isPressed.value ? 1.05 : 1) },
+        { rotate: `${props.rotate}deg` },
+      ],
+      // position: 'absolute',
+      backgroundColor: isPressed.value ? 'yellow' : props.backgroundColour,
+    };
+  });
   return (
-    <View
-      style={[
-        styles.card,
-        { backgroundColor: props.backgroundColour, transform: props.transform },
-      ]}
-    >
-      <Text>card</Text>
-    </View>
+    <GestureDetector gesture={gesture}>
+      <Animated.View style={[styles.recipeCard, animatedStyles]}>
+        <Text style={styles.receipeTitle}>Title: {props.data.title}</Text>
+      </Animated.View>
+    </GestureDetector>
   );
 }
-
 const styles = StyleSheet.create({
-  card: {
-    position: 'absolute',
+  recipeCard: {
     width: 300,
     height: 600,
     borderRadius: 30,
     alignSelf: 'center',
     alignContent: 'center',
+  },
+  receipeTitle: {
+    alignSelf: 'center',
+    fontSize: 30,
+    paddingTop: 50,
   },
 });
--- components/Recipes.tsx
+++ components/Recipes.tsx
@@ -1,24 +1,58 @@
-import { View, Text, StyleSheet } from 'react-native';
-import RecipeCardTop from './RecipeCardTop';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  StyleProp,
+  ViewStyle,
+} from 'react-native';
 import RecipeCard from './RecipeCard';
+import { useState } from 'react';
+
+export type card = {
+  id: number;
+  title: string;
+  colour: string;
+};
 
 export default function Recipes() {
+  const [index, setIndex] = useState(0);
+  const cards = [
+    { id: 1, title: 'recipe 1', colour: 'green' },
+    { id: 2, title: 'recipe 2', colour: 'pink' },
+    { id: 3, title: 'recipe 3', colour: 'blue' },
+    { id: 4, title: 'recipe 4', colour: 'red' },
+  ];
   return (
-    <View style={styles.cardList}>
-      <RecipeCard
-        backgroundColour={'green'}
-        transform="rotate(5deg)"
-      ></RecipeCard>
-      <RecipeCard
-        backgroundColour={'pink'}
-        transform="rotate(-5deg)"
-      ></RecipeCard>
-      <RecipeCardTop></RecipeCardTop>
+    <View>
+      {/* 
+      <View style={styles.cardList}>
+        <RecipeCard
+          backgroundColour={{ backgroundColor: 'green' }}
+          transform={{ transform: [{ rotate: '5deg' }] }}
+        ></RecipeCard>
+        <RecipeCard
+          backgroundColour={{ backgroundColor: 'pink' }}
+          transform={{ transform: [{ rotate: '-5deg' }] }}
+        ></RecipeCard>
+        <RecipeCardTop backgroundColour=""></RecipeCardTop>
+      </View> 
+      */}
+      <View style={styles.cardList}>
+        {cards.slice(0, 3).map((card, i) => (
+          <RecipeCard
+            key={card.id}
+            data={card}
+            backgroundColour={card.colour}
+            isTopCard={i === index}
+            rotate={5 - 5 * i}
+          ></RecipeCard>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   cardList: {},
-  cards: { position: 'absolute' },
 });
