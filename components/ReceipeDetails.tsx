6c7b24bc21835504e8f0fa3d78dd48a8731e970a
CapSap <cshotam@gmail.com>
2026-04-05 12:37:36 +1000

    add the main button / heading on the details screen and copy over the styling from receipes

--- components/ReceipeDetails.tsx
+++ components/ReceipeDetails.tsx
@@ -2,6 +2,7 @@ import { useNavigationEvent } from 'navigation-react';
 import { StyleSheet, Text, View } from 'react-native';
 import { cards } from '../assets/data';
 import { RecipeNavigator } from './Types';
+import MunchLogo from './MunchLogo';
 
 export default function RecipeDetails() {
   const { data } = useNavigationEvent<RecipeNavigator, 'detail'>();
@@ -10,6 +11,12 @@ export default function RecipeDetails() {
 
   return (
     <View style={styles.container}>
+      <MunchLogo />
+
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerText}>+ Recipe Card</Text>
+      </View>
+
       <Text style={styles.text}>card id from navigation: {data.cardId}</Text>
       <Text style={styles.text}>title: {card?.title}</Text>
     </View>
@@ -24,5 +31,22 @@ const styles = StyleSheet.create({
   text: {
     alignSelf: 'center',
     fontSize: 30,
+  },
+  headerContainer: {
+    marginTop: 45,
+    marginLeft: 44,
+  },
+  headerText: {
+    borderWidth: 2,
+    borderColor: '#8A66D0',
+    alignSelf: 'flex-start',
+    borderRadius: 14,
+    backgroundColor: '#f3f3f3',
+    color: '#8a66d0',
+    fontSize: 36,
+    padding: 7,
+    fontWeight: 'bold',
+    paddingLeft: 27,
+    paddingRight: 27,
   },
 });
